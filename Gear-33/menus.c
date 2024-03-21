4a714bb7562d4b0f38f32123376fb8969576b6d4
UnaiE <unai.egusquizadel@opendeusto.es>
2024-03-21 18:56:57 +0100

    Añadidas más funcionalidades de los menus de usuario & completar un poco más usuario.h / usuario.c

--- Gear-33/menus.c
+++ Gear-33/menus.c
@@ -361,25 +361,25 @@ void menuEliminarCoche(int* opcion) {
 }
 
 
-void menuGestUsuario(int* opcion){
-	dibujoPersona();
-	 printf(
-	        "---------------------------\n\n"
-	        "    Gestionar usuario\n\n"
-	        "---------------------------\n\n"
-	        "1. Anadir usuario\n"
-	        "2. Modificar usuario\n"
-	        "3. Eliminar usuario\n"
-	        "0. Volver\n\n"
-			"Introduce una opcion: ");
+	void menuGestUsuario(int* opcion){
+		dibujoPersona();
+		printf(
+				"---------------------------\n\n"
+				"    Gestionar usuario\n\n"
+				"---------------------------\n\n"
+				"1. Anadir usuario\n"
+				"2. Modificar usuario\n"
+				"3. Eliminar usuario\n"
+				"0. Volver\n\n"
+				"Introduce una opcion: ");
 
-	fflush(stdout);
-	fflush(stdin);
-	scanf("%d",opcion);
-	opcionMenuGestUsuario(opcion);
-}
-void opcionMenuGestUsuario(int *opcion) {
-	system("cls");
+		fflush(stdout);
+		fflush(stdin);
+		scanf("%d",opcion);
+		opcionMenuGestUsuario(opcion);
+	}
+	void opcionMenuGestUsuario(int *opcion) {
+		system("cls");
 		switch (*opcion) {
 				case 1:
 					menuAnadirUsuario(opcion);
@@ -398,85 +398,201 @@ void opcionMenuGestUsuario(int *opcion) {
 					menuGestUsuario(opcion);
 					break;
 			}
-}
-void menuAnadirUsuario(int* opcion) {
-	dibujoPersona();
-	printf(
-	"---------------------------\n\n"
-	"       Registro de usuario\n\n"
-	"---------------------------\n\n");
-	printf("Introduce el nombre \n");
-		fflush(stdout);
-		fflush(stdin);
+	}
+	void menuAnadirUsuario(int* opcion) {
+		dibujoPersona();
+		printf(
+				"---------------------------\n\n"
+				"       Registro de usuario\n\n"
+				"---------------------------\n\n");
+		Usuario u = pedirUsuario();
 
-
-		printf("Introduce el apellido \n");
-		fflush(stdout);
-		fflush(stdin);
-
-
-		printf("Introduce el dni \n");
-		fflush(stdout);
-		fflush(stdin);
-
-
-		printf("Introduce la fecha de nacimiento DD/MM/AAAA \n");
-		fflush(stdout);
-		fflush(stdin);
-
-
-		printf("Introduce el teléfono \n");
-		fflush(stdout);
-		fflush(stdin);
-
-
-		printf("Introduce la dirección \n");
-		fflush(stdout);
-		fflush(stdin);
-
-
-		printf("Introduce la contraseña \n");
-		fflush(stdout);
-		fflush(stdin);
 		menuGestUsuario(opcion);
 }
-void menuModificarUsuario(int* opcion) {
+	void menuModificarUsuario(int* opcion) {
 
-
-	dibujoPersona();
-	printf(
-	"---------------------------\n\n"
-	"      Modificar datos\n\n"
-	"---------------------------\n\n");
-	printf("Introduzca el dni:  ");
-	fflush(stdout);
-	fflush(stdin);
+		char dni[9] = "";
+		dibujoPersona();
+		printf(
+				"---------------------------\n\n"
+				"      Modificar datos\n\n"
+				"---------------------------\n\n");
+		printf("Introduzca el dni:  ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(dni);
 
 
 	//falta funcion que comprueba si el dni existe
 
 	//
-	fflush(stdout);
-	fflush(stdin);
-	printf("\n1.Modificar nombre\n"
-	       "2.Modificar apellido\n"
-	       "3.Modificar modelo\n"
-	       "4.Modificar DNI\n"
-	       "5.Modificar fecha de nacimiento\n"
-	       "6.Modificar teléfono\n"
-	       "7.Modificar dirección\n"
-	       "8.Modificar contraseña\n"
-	       "0.Volver\n\n"
-		   "Introduce una opción: ");
+		fflush(stdout);
+		fflush(stdin);
+		printf("\n1.Modificar nombre\n"
+				"2.Modificar apellido\n"
+				"3.Modificar DNI\n"
+				"4.Modificar fecha de nacimiento\n"
+				"5.Modificar teléfono\n"
+				"6.Modificar dirección\n"
+				"7.Modificar contraseña\n"
+				"0.Volver\n\n"
+				"Introduce una opción: ");
 
-	fflush(stdout);
-	fflush(stdin);
-	scanf("%d", opcion);
+		fflush(stdout);
+		fflush(stdin);
+		scanf("%d", opcion);
+		opcionMenuModificarUsuarios(opcion);
 
 }
+
+	void opcionMenuModificarUsuarios(int *opcion) {
+		system("cls");
+			switch (*opcion) {
+				case 1:
+					menuModificarNombre(opcion);
+					break;
+				case 2:
+					menuModificarApellido(opcion);
+					break;
+				case 3:
+					menuModificarDni(opcion);
+					break;
+				case 4:
+					menuModificarFecha(opcion);
+					break;
+				case 5:
+					menuModificarTelefono(opcion);
+					break;
+				case 6:
+					menuModificarDireccion(opcion);
+					break;
+				case 7:
+					menuModificarContraseña(opcion);
+					break;
+				case 0:
+					menuGestUsuario(opcion);
+					break;
+				default:
+					printf("El digito introducido no corresponde a ninguno de los anteriores\n");
+					menuModificarUsuario(opcion);
+					break;
+			}
+}
+	void menuModificarNombre(int *opcion) {
+		char nombre[];
+
+		dibujoPersona();
+		printf(
+				"---------------------------\n\n"
+				"    Modificar nombre\n\n"
+				"---------------------------\n\n");
+		printf("Introduzca el nuevo nombre:  ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(nombre);
+	// crear funcion para modificar el nombre
+	/////
+		menuGestUsuario(opcion);
+	}
+	void menuModificarApellido(int *opcion) {
+		char apellido[];
+
+		dibujoPersona();
+		printf(
+		"---------------------------\n\n"
+		"    Modificar apellido\n\n"
+		"---------------------------\n\n");
+		printf("Introduzca el nuevo apellido:  ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(apellido);
+		// crear funcion para modificar el apellido
+		/////
+		menuGestUsuario(opcion);
+	}
+	void menuModificarDni(int *opcion) {
+			char dni[9];
+
+			dibujoPersona();
+			printf(
+			"---------------------------\n\n"
+			"    Modificar dni\n\n"
+			"---------------------------\n\n");
+			printf("Introduzca el nuevo dni:  ");
+			fflush(stdout);
+			fflush(stdin);
+			gets(dni);
+			// crear funcion para modificar el dni
+			/////
+			menuGestUsuario(opcion);
+		}
+	void menuModificarFecha(int *opcion) {
+				char fecha[];
+
+				dibujoPersona();
+				printf(
+				"---------------------------\n\n"
+				"    Modificar la fecha de nacimiento\n\n"
+				"---------------------------\n\n");
+				printf("Introduzca la nueva fecha de nacimiento:  ");
+				fflush(stdout);
+				fflush(stdin);
+				gets(fecha);
+				// crear funcion para modificar la fecha de nacimiento
+				/////
+				menuGestUsuario(opcion);
+			}
+	void menuModificarTelefono(int *opcion) {
+				char telefono[9];
+				dibujoPersona();
+				printf(
+				"---------------------------\n\n"
+				"    Modificar el teléfono\n\n"
+				"---------------------------\n\n");
+				printf("Introduzca el nuevo teléfono:  ");
+				fflush(stdout);
+				fflush(stdin);
+				gets(telefono);
+				// crear funcion para modificar el teléfono
+				/////
+				menuGestUsuario(opcion);
+				}
+	void menuModificarDireccion(int *opcion) {
+		char direccion[];
+
+		dibujoPersona();
+		printf(
+		"---------------------------\n\n"
+		"    Modificar la dirección\n\n"
+		"---------------------------\n\n");
+		printf("Introduzca la nueva dirección:  ");
+		fflush(stdout);
+		fflush(stdin);
+		gets(direccion);
+		// crear funcion para modificar la dirección
+		/////
+		menuGestUsuario(opcion);
+	}
+	void menuModificarContraseña(int *opcion) {
+			char contrasena[];
+
+			dibujoPersona();
+			printf(
+			"---------------------------\n\n"
+			"    Modificar la contraseña\n\n"
+			"---------------------------\n\n");
+			printf("Introduzca la neuva contraseña:  ");
+			fflush(stdout);
+			fflush(stdin);
+			gets(contrasena);
+			// crear funcion para modificar la contraseña
+			/////
+			menuGestUsuario(opcion);
+		}
+
 void menuEliminarUsuario(int* opcion) {
 
-
+	char dni[9]= "";
 	dibujoPersona();
 	printf(
 	"---------------------------\n\n"
@@ -485,6 +601,8 @@ void menuEliminarUsuario(int* opcion) {
 	printf("Introduzca el dni:  ");
 	fflush(stdout);
 	fflush(stdin);
+	gets(dni);
+	opcionMenuGestUsuario(opcion);
 
 }
 
--- Gear-33/usuario.h
+++ Gear-33/usuario.h
@@ -8,3 +8,31 @@
 int inicioSesion(char dni[], char contrasena[]);
 
 #endif /* USUARIO_H_ */
+
+#include "sqlite3.h"
+
+
+	//Usuario
+	typedef struct {
+		char nombre[];
+		char apellido[];
+		char dni[9];
+		char fecha[];
+		char telefono[9];
+		char direccion[];
+		char contrasena[];
+
+
+	}Usuario;
+
+	//lista de usuarios
+	typedef struct {
+		Usuario *lUsuarios;
+		int tam;
+		int numUsuarios;
+
+	}ListaUsuarios;
+
+	int inicioSesion(char dni[], char contrasena[]);
+	void aniadirUsuario(ListaUsuarios *lu, Usuario u);
+
